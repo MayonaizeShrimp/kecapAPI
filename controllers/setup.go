90c0c0501250486b288cc5b3e706631ca9bd1dcf
MayonaizeShrimp <85448449+MayonaizeShrimp@users.noreply.github.com>
2021-12-22 10:14:02 +0700

    adding cors
    set gin to allow cors

--- controllers/setup.go
+++ controllers/setup.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"time"
+
+	cors "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +12,18 @@ var router *gin.Engine
 //Initiate Gin
 func SetRouting() {
 	router = gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://127.0.0.1:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+	router.Run()
 }
 
 //Set routes for login
